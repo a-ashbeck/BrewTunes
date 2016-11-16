a7e5115ff7d4f9178f86b431641863ab9af297fb
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-11-15 19:48:02 -0600

    Add firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// The business
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCtdaFgW6hk1SB-pasoWL19J8HuzeSZSo0",
+  authDomain: "beertunes-36bca.firebaseapp.com",
+  databaseURL: "https://beertunes-36bca.firebaseio.com",
+  storageBucket: "beertunes-36bca.appspot.com",
+  messagingSenderId: "144484245094"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
