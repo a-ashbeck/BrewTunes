5e212b92cf4f470de973bec45598a627fbd3ec1a
maxvanbel <Max@Maximes-MacBook-Pro.local>
2016-11-19 14:25:55 -0600

    loading screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,13 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCtdaFgW6hk1SB-pasoWL19J8HuzeSZSo0",
-  authDomain: "beertunes-36bca.firebaseapp.com",
-  databaseURL: "https://beertunes-36bca.firebaseio.com",
-  storageBucket: "beertunes-36bca.appspot.com",
-  messagingSenderId: "144484245094"
+    apiKey: "AIzaSyCtdaFgW6hk1SB-pasoWL19J8HuzeSZSo0",
+    authDomain: "beertunes-36bca.firebaseapp.com",
+    databaseURL: "https://beertunes-36bca.firebaseio.com",
+    storageBucket: "beertunes-36bca.appspot.com",
+    messagingSenderId: "144484245094"
 };
 firebase.initializeApp(config);
-
 var database = firebase.database();
-
 // some global variables
 var input = '';
 var beerID = '';
@@ -22,86 +20,105 @@ var playlistName = '';
 var searches = {};
 
 function hidePlaylist() {
-	$('#playlist').hide();
+    $('#playlist').hide();
+}
+
+function hideLoad() {
+    $('#load').hide();
 }
 
 function showPlaylist() {
-	$('#playlist').show();
+    $('#playlist').show();
+}
+
+function showLoad() {
+    $('#load').show();
 }
 
 function resetFormField() {
-	$('#input-beer').val('');
+    $('#input-beer').val('');
 }
 
 function setPlaylistURL() {
-	console.log('set: ' + playlistURL)
-	$('iframe').attr('src', playlistURL);
+    console.log('set: ' + playlistURL)
+    $('iframe').attr('src', playlistURL);
 }
 
 function displayInputBeer() {
-	$('#entered-beer').html(input);
+    $('#entered-beer').html(input);
 }
-
 // Untappd API
 var CLIENTID = 'A9F55BC3824C82CA5DC0FBC018240C4E61ED09A1';
 var CLIENTSECRET = '4C79983D9CBA23DE70A551F209EBAE2512A0BE8C';
+
 function initialQueryString(beerInput) {
-  return 'search/beer?q=' + beerInput + '&';
+    return 'search/beer?q=' + beerInput + '&';
 }
+
 function specificQueryString(beerID) {
-  return 'beer/info/' + beerID + '?';
+    return 'beer/info/' + beerID + '?';
 }
+
 function queryURL(queryString) {
-  return 'https://api.untappd.com/v4/' + queryString + 'client_id=' + CLIENTID +
-         '&client_secret=' + CLIENTSECRET;
+    return 'https://api.untappd.com/v4/' + queryString + 'client_id=' + CLIENTID + '&client_secret=' + CLIENTSECRET;
 }
 
-$(document).on('ready', function() {
-	hidePlaylist();
-
-  $(document).on('click', '#submit', function() {
-  	input = $('#input-beer').val().trim();
-  	displayInputBeer();
-    fetchBeers(input);
-
+function load() {
+    showLoad();
+    $('#liquid') // I Said Fill 'Er Up!
+        .delay(800).animate({
+            height: '190px'
+        }, 2500);
+    $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
+        .hide().delay(3000).animate({
+            bottom: '200px'
+        }, 0).fadeIn(1000).show(0);
     return false;
-  });
+}
+$(document).on('ready', function () {
+    hidePlaylist();
+    hideLoad();
+    $('.beer-foam').hide();
+    $(document).on('click', '#submit', function () {
+        load();
+        input = $('#input-beer').val().trim();
+        displayInputBeer();
+        fetchBeers(input);
+        return false;
+    });
 });
 
 function fetchBeers(input) {
-  $.ajax({
-    type: 'GET',
-    url: queryURL(initialQueryString(input.replace(' ', '+'))),
-    dataType: 'json'
-  }).done(function(response) {
-    var beer = response.response.beers.items[0];
-    beerID = beer.beer.bid;
-    beerName = beer.beer.beer_name;
-    console.log(beerID);
-    console.log(beerName);
-
-    fetchSpecificBeer(beerID);
-  });
+    $.ajax({
+        type: 'GET',
+        url: queryURL(initialQueryString(input.replace(' ', '+'))),
+        dataType: 'json'
+    }).done(function (response) {
+        var beer = response.response.beers.items[0];
+        beerID = beer.beer.bid;
+        beerName = beer.beer.beer_name;
+        console.log(beerID);
+        console.log(beerName);
+        fetchSpecificBeer(beerID);
+    });
 }
 
-function fetchSpecificBeer(beerID){
-  $.ajax({
-    type: 'GET',
-    url: queryURL(specificQueryString(beerID)),
-    dataType: 'json'
-  }).done(function(response) {
-    ratingCount = response.response.beer.rating_count;
-    ratingScore = response.response.beer.rating_score;
-    console.log(ratingCount);
-    console.log(ratingScore);
-
-    if (ratingCount && ratingScore) {
-      genres = ['indie', 'rock', 'country', 'metal'];
-      genre = genres[Math.floor(Math.random() * 4)];
-    }
-
-    fetchPlaylist(genre);
-  });
+function fetchSpecificBeer(beerID) {
+    $.ajax({
+        type: 'GET',
+        url: queryURL(specificQueryString(beerID)),
+        dataType: 'json'
+    }).done(function (response) {
+        ratingCount = response.response.beer.rating_count;
+        ratingScore = response.response.beer.rating_score;
+        console.log(ratingCount);
+        console.log(ratingScore);
+        if (ratingCount && ratingScore) {
+            genres = ['indie', 'rock', 'country', 'metal'];
+            genre = genres[Math.floor(Math.random() * 4)];
+        }
+        fetchPlaylist(genre);
+    });
 }
 
 function fetchPlaylist(genre) {
